470fbeca43388e912517b4470aab930791f11c76
Takao <sukisya70@gmail.com>
2019-01-05 15:15:57 +0900

    slot-scope

--- slot/slot2.js
+++ slot/slot2.js
@@ -6,5 +6,31 @@ var TodoList = {
     }
   },
   template: `
+    <ul>
+      <template v-for="todo in todos">
+        <slot :todo="todo">
+          <li :key="todo.id">
+            {{ todo.text }}
+          </li>
+        </slot>
+      </template>
+    </ul>
   `
 }
+
+new Vue({
+  el: '#app',
+  data: function () {
+    return {
+      todos: [
+        { id: 1, text: 'C++', isCompleted: true },
+        { id: 2, text: 'JavaScript', isCompleted: true },
+        { id: 3, text: 'Java', isCompleted: true },
+        { id: 4, text: 'Ruby', isCompleted: true }
+      ]
+    }
+  },
+  components: {
+    TodoList: TodoList
+  }
+})
